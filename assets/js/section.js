f11b52c53a12d50f1ac568a101886a0ff05f6977
PARKSUNGWOO-0922 <mailcrybaby@gmail.com>
2026-01-12 17:58:25 +0900

    section테스트중

--- assets/js/section.js
+++ assets/js/section.js
@@ -1,16 +1,16 @@
 
   const menuImages = document.querySelectorAll('.main-icon-box img');
 
-  // menuImages.forEach(img => {
-  //   img.addEventListener('mouseenter', () => {
-  //     img.src = img.dataset.hover;
-  //   });
+  menuImages.forEach(img => {
+    img.addEventListener('mouseenter', () => {
+      img.src = img.dataset.hover;
+    });
 
-  //   img.addEventListener('mouseleave', () => {
-  //     img.src = img.dataset.default;
-  //   });
-  //   console.log(menuImages.length);
-  // });
+    img.addEventListener('mouseleave', () => {
+      img.src = img.dataset.default;
+    });
+    console.log(menuImages.length);
+  });
 
  /*
  img.src와 dataset 경로는 브라우저가 보는 방식이 달라서 바로 비교하면 안 됨.
@@ -19,30 +19,28 @@ endsWith() 사용하거나
 
 상태 변수로 토글하는 방식이 안전함. 
   */ 
-//  menuImages.forEach(img => {
-//   img.addEventListener('click', () => {
-//     if (img.src === img.dataset.default) {
-//       img.src = img.dataset.hover;
-//     } else {
-//       img.src = img.dataset.default;
-//     }
-//   });
-
-//   console.log(menuImages.length);
-// });
-
-menuImages.forEach(img => {
-  let toggled = false; // 상태 저장
+ menuImages.forEach(img => {
   img.addEventListener('click', () => {
-    if (!toggled) {
+    if (img.src === img.dataset.default) {
       img.src = img.dataset.hover;
-      toggled = true;
     } else {
       img.src = img.dataset.default;
-      toggled = false;
     }
   });
-  console.log('dataset'+img.dataset);
-  console.log('default'+img.dataset.default); // "default.png"
-   console.log('hover'+img.dataset.hover);   // "hover.png"
+
+  console.log(menuImages.length);
 });
+
+// menuImages.forEach(img => {
+//     let toggled = false; // 상태 저장
+//     img.addEventListener('click', () => {
+//       if (!toggled) {
+//         img.src = img.dataset.hover;
+//         toggled = true;
+//       } else {
+//         img.src = img.dataset.default;
+//         toggled = false;
+//       }
+//     });
+  
+// });
